92296ca18df0fcd39e55b777a5c87e6620a71366
sourav sharma <souravsh1234567@gmail.com>
2023-06-16 17:06:31 +0530

    added code

--- src/components/apps/VSCode.tsx
+++ src/components/apps/VSCode.tsx
@@ -2,7 +2,7 @@ export default function VSCode() {
   return (
     <iframe
       className="h-full w-full bg-vscode"
-      src="https://github1s.com/Renovamen/playground-macos/blob/main/README.md"
+      src="https://github1s.com/souravprogrammer/macos-portfolio"
       title="VSCode"
     />
   );
